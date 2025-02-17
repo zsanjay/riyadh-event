1b2d653081656cae8083ec6b4005802f9b392e4c
priyanka <priyankaprakashchand@gmail.com>
2025-02-17 14:11:32 +0400

    update title icon and name and remove unused fonts

--- riyadh-event-project/src/app/page.js
+++ riyadh-event-project/src/app/page.js
@@ -1,14 +1,10 @@
 import Header from '@/components/header';
 import WhyRiyadh from '@/components/why-riyadh';
 import EventHighlights from '@/components/event-highlights';
-import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/images/rising-riyadh-logo.svg" />
-      </Head>
       <div className="m-auto bg-custom-black h-full w-full">
         <div className="m-auto w-full ">
           <Header />
